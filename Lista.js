33eafc5a7f7c1c77e8755ae065e1d0889b4642bc
IsshinTheSwordSaint <ut_4003@zsp3zamosc.pl>
2024-10-22 09:24:49 +0200

    dodawanie

--- Lista.js
+++ Lista.js
@@ -1,6 +1,10 @@
-console.log("Część 1");
-console.log("------------------------")
 let lista = {
-    name: "Maxwell Wright",
-    phone:
+    name: "Maxwell Wright" ,
+    phone: "(0191) 719 6496" ,
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+},
+{
+    name: "Raja Villarreal" ,
+    phone: "0866 398 2895"
+
 }
