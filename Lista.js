71e38368a4450330d8f2bdeba3b00e89efca7633
IsshinTheSwordSaint <ut_4003@zsp3zamosc.pl>
2024-10-22 09:35:45 +0200

    dodanie

--- Lista.js
+++ Lista.js
@@ -1,10 +1,27 @@
-let lista = {
-    name: "Maxwell Wright" ,
-    phone: "(0191) 719 6496" ,
-    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
-},
-{
-    name: "Raja Villarreal" ,
-    phone: "0866 398 2895"
+let lista = [
+    {
+        name: "Maxwell Wright" ,
+        phone: "(0191) 719 6496" ,
+        email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+    },
+    {
+        name: "Raja Villarreal" ,
+        phone: "0866 398 2895",
+        email: "posuere.vulputate@sed.com"
+    },
+    {
+        name: "Helen Richards" ,
+        phone: "0800 1111" ,
+        email: "libero@convalis.edu"
+    }
+];
+let kontakt = {
+    name: "Maise Haley" ,
+    phone: "0913 531 3030" ,
+    email: "risus.Quisque@urna.ca"
+};
+lista.push(kontakt);
 
-}
+
+
+
